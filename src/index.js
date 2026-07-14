cbaf424a4aa7db5b6a4b8f814a10803ff7a01f6c
Hycaro Phillype <phillypehycaro@gmail.com>
2026-07-14 19:55:25 -0300

    Dando ínicio ao simulador de Mario Kart

---
+++ src/index.js
@@ -0,0 +1,47 @@
+const player1 = {
+  NOME: "Mario",
+  VELOCIDADE: 8,
+  MANOBRABLIDADE: 6,
+  PODER: 6,
+  PONTOS: 0,
+};
+
+const player2 = {
+  NOME: "Luigi",
+  VELOCIDADE: 6,
+  MANOBRABLIDADE: 8,
+  PODER: 8,
+  PONTOS: 0,
+};
+
+const player3 = {
+  NOME: "Peach",
+  VELOCIDADE: 6,
+  MANOBRABLIDADE: 8,
+  PODER: 4,
+  PONTOS: 0,
+};
+
+const player4 = {
+  NOME: "Bowser",
+  VELOCIDADE: 10,
+  MANOBRABLIDADE: 4,
+  PODER: 10,
+  PONTOS: 0,
+};
+
+const player5 = {
+  NOME: "Yoshi",
+  VELOCIDADE: 4,
+  MANOBRABLIDADE: 8,
+  PODER: 3,
+  PONTOS: 0,
+};
+
+const player6 = {
+  NOME: "Donkey Kong",
+  VELOCIDADE: 4,
+  MANOBRABLIDADE: 4,
+  PODER: 10,
+  PONTOS: 0,
+};
