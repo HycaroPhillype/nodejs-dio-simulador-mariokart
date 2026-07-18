ed1483733508ebe54b2fe0e93a8d14693cba9e04
Hycaro Phillype <phillypehycaro@gmail.com>
2026-07-18 15:15:55 -0300

    Fix: Correção das condições, movidas para dentro do for, e atalizei a descrição do readme.md

--- src/index.js
+++ src/index.js
@@ -50,9 +50,87 @@ async function rollDice() {
   return Math.floor(Math.random() * 6) + 1;
 }
 
+async function getRandomBlock() {
+  let random = Math.random();
+  // console.log(`O NÚMERO SORTEADO FOI: ${random}\n`);
+  let result;
+
+  switch (true) {
+    case random < 0.33:
+      result = "RETA";
+      break;
+    case random < 0.66:
+      result = "CURVA";
+      break;
+    default:
+      result = "CONFRONTO";
+  }
+  return result;
+}
+
+async function logRollResult(characterName, block, diceResult, attribute) {
+  console.log(
+    `${characterName} 🎲 rolou o dado de ${block} ${diceResult} + ${attribute} = ${diceResult + attribute} `,
+  );
+}
+
 async function playRaceEngine(charater1, charater2) {
   for (let round = 1; round <= 5; round++) {
-    console.log(`\n🏁🚦 RODADA ${round} COMEÇANDO...\n`);
+    console.log(` 🏁🚦RODADA ${round} COMEÇANDO...`);
+
+    let block = await getRandomBlock();
+    console.log(`O BLOCO SORTEADO FOI: ${block}\n`);
+
+    // ROLAR DADOS
+
+    let diceResult1 = await rollDice();
+    let diceResult2 = await rollDice();
+
+    // TESTE DE HABILIDADE
+
+    let toltalTestSkill1 = 0;
+    let toltalTestSkill2 = 0;
+
+    if (block === "RETA") {
+      toltalTestSkill1 = diceResult1 + charater1.VELOCIDADE;
+      toltalTestSkill2 = diceResult2 + charater2.VELOCIDADE;
+
+      await logRollResult(
+        charater1.NOME,
+        "velocidade",
+        diceResult1,
+        charater1.VELOCIDADE,
+      );
+      await logRollResult(
+        charater2.NOME,
+        "velocidade",
+        diceResult1,
+        charater2.VELOCIDADE,
+      );
+    }
+
+    if (block === "CURVA") {
+      toltalTestSkill1 = diceResult1 + charater1.MANOBRABLIDADE;
+      toltalTestSkill2 = diceResult2 + charater2.MANOBRABLIDADE;
+
+      await logRollResult(
+        charater1.NOME,
+        "manobrabilidade",
+        diceResult1,
+        charater1.MANOBRABLIDADE,
+      );
+      await logRollResult(
+        charater2.NOME,
+        "manobrabilidade",
+        diceResult1,
+        charater2.MANOBRABLIDADE,
+      );
+    }
+
+    if (block === "CONFRONTO") {
+      let powerResult1 = diceResult1 + charater1.PODER;
+      let powerResult2 = diceResult2 + charater2.PODER;
+    }
   }
 }
 
